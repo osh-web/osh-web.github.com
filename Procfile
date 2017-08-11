871e9e3159a646fa66148b725854d610eeb5bd8d
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-11 17:56:26 +0900

    不要なファイルを削除

--- Procfile
+++
@@ -1,2 +0,0 @@
-server: browser-sync start --files _site --server _site
-generate: bundle exec jekyll build -w
--- src/2017/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { combineReducers } from 'redux'
-
-import window from './windowReducer'
-import speakerDialog from './speakerDialogReducer'
-
-export default combineReducers({ window, speakerDialog })
