115fd3ee7543b6680d6bccaf76d88278286e2e9d
Tomohiko Himura <eiel.hal@gmail.com>
2017-10-16 21:16:37 +0900

    Updates

---
+++ react-static-routes.js
@@ -0,0 +1,17 @@
+
+    import React, { Component } from 'react'
+    import { Switch, Route } from 'react-router-dom'
+
+    
+
+    export default class Routes extends Component {
+      render () {
+        return (
+          <Switch>
+              
+              
+          </Switch>
+        )
+      }
+    }
+  
