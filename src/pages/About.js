46a34cd3063bb60d95e151649f4740439e38ab12
Tomohiko Himura <eiel.hal@gmail.com>
2018-12-28 22:02:11 +0900

    fix: Aboutページをgridレイアウト化

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Head } from 'react-static'
-import { Flex, Box } from 'grid-styled'
+import { Cell, Grid, Row } from '@material/react-layout-grid'
+import { Headline3, Body1 } from '@material/react-typography'
 
-import BackButton from '../components/BackButton'
-import H1 from '../components/2018/H1'
-import H2 from '../components/2018/H2'
 import { AppBar } from '../organisms/AppBar'
 
 const p1 = `
@@ -16,37 +14,39 @@ const p2 = `
 企画と運営は各地域の技術系ユーザコミュニティのボランティアによって行われています。
 `
 
-const Title = ({ children }) => <H1 f={[4, 6]}>{children}</H1>
-
 export default () => (
   <div>
     <Head>
       <title>初めての方へ - オープンセミナー広島</title>
     </Head>
     <AppBar title="初めての方へ">
-      <H2>オープンセミナーとは</H2>
-      <Flex justify="center">
-        <Box mx={[16, 200]}>
-          {p1}
-          {p2}
-        </Box>
-      </Flex>
-      <H2>オープンセミナー広島について</H2>
-      <Flex justify="center">
-        <Box mx={[16, 200]}>
-          <p>
-            広島では毎年2月ごろにオープンセミナーを開催をしています。
-            実行委員長を毎年交代をし、運営のノウハウを引き継ぎ、新たな実行委員会を毎年立ち上げています。
-            実行委員会では、ITコミュニティに積極的に参加シている人や、コミュニティを立ち上げている人たちが主となっています。
-            そのため、コミュニティの年に一度のお祭りのようなものとなっています。
-          </p>
-          <p>
-            実行委員会では、一緒にイベントを作りあげたい仲間を探しています。
-            コミュニティ同士の繋がりの強くしたい方や、コミュニティの主催者と仲良くしたい方におすすめです。
-            参加ご希望の方は気軽にご連絡ください。
-          </p>
-        </Box>
-      </Flex>
+      <Grid>
+        <Row>
+          <Cell columns={6}>
+            <Headline3 tag="h1">オープンセミナーとは</Headline3>
+            <Body1>
+              {p1}
+              {p2}
+            </Body1>
+          </Cell>
+          <Cell columns={6}>
+            <Headline3 tag="h1">オープンセミナー広島について</Headline3>
+            <Body1>
+              <p>
+                広島では毎年2月ごろにオープンセミナーを開催をしています。
+                実行委員長を毎年交代をし、運営のノウハウを引き継ぎ、新たな実行委員会を毎年立ち上げています。
+                実行委員会では、ITコミュニティに積極的に参加シている人や、コミュニティを立ち上げている人たちが主となっています。
+                そのため、コミュニティの年に一度のお祭りのようなものとなっています。
+              </p>
+              <p>
+                実行委員会では、一緒にイベントを作りあげたい仲間を探しています。
+                コミュニティ同士の繋がりの強くしたい方や、コミュニティの主催者と仲良くしたい方におすすめです。
+                参加ご希望の方は気軽にご連絡ください。
+              </p>
+            </Body1>
+          </Cell>
+        </Row>
+      </Grid>
     </AppBar>
   </div>
 )
