a6c2716b99a32e5279a5cf28889f178ed8fa8ab6
Tomohiko Himura <eiel.hal@gmail.com>
2018-12-29 00:10:56 +0900

    fix: 講師カードのデザイン修正

--- src/organisms/2019/Speakers.js
+++ src/organisms/2019/Speakers.js
@@ -11,21 +11,19 @@ import {
 } from '@material/react-typography'
 
 const Speaker = ({ name, furi, description, id, title, company }) => (
-  <Card
-    className="mdc-elevation--z1"
-    outlined
-    style={{ margin: 4, padding: 16 }}
-  >
+  <Card className="mdc-elevation--z1" outlined>
     <CardMedia imageUrl={`/img/2019/${id}.jpg`} square />
-    <Headline1 style={{ fontSize: 18, lineHeight: 1 }}>
-      セッション内容 TBD
-    </Headline1>
-    <Caption>{furi} </Caption>
-    <Headline1 style={{ fontSize: 18, lineHeight: 1 }}>
-      {name} <Button href={company.uri}>{company.name}</Button>
-    </Headline1>
-    <Body2>{description}</Body2>
-    <Body2>{title}</Body2>
+    <div style={{ padding: '0 16px' }}>
+      <Headline1 style={{ fontSize: 18, lineHeight: 1 }}>
+        セッション内容 TBD
+      </Headline1>
+      <Caption>{furi} </Caption>
+      <Headline1 style={{ fontSize: 18, lineHeight: 1 }}>
+        {name} <Button href={company.uri}>{company.name}</Button>
+      </Headline1>
+      <Body2>{description}</Body2>
+      <Body2>{title}</Body2>
+    </div>
   </Card>
 )
 
