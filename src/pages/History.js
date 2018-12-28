f6bb34ca0f40c33be802da7d82dabd44c0d7b9c5
Tomohiko Himura <eiel.hal@gmail.com>
2018-12-28 14:21:55 +0900

    fix: これまでのオープンセミナーのページのレイアウト崩れを修正

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,44 +1,33 @@
-// @flow
+// @flow strict
 import React from 'react'
-import { Box, Flex } from 'grid-styled'
-import { Link } from '@reach/router'
+import Card from '@material/react-card'
+import { Cell, Grid, Row } from '@material/react-layout-grid'
+import { navigate } from '@reach/router'
 
-import { BackButton } from '../components'
-import H1 from '../components/2018/H1'
 import { seminarList } from '../resources'
 import { AppBar } from '../organisms/AppBar'
 
-const BoxWrap = Box.extend`
-  color: black;
-  text-decoration: none;
-`
-
 const Seminar = ({ name, theme, url }) => (
-  <BoxWrap
-    is="a"
-    href={url}
-    width={[1, 0.48]}
-    py={20}
-    pl={[6, 20]}
-    f={[1, 4]}
-    mb={[20, 50]}
-    mr={10}
+  <Card
+    outlined={true}
+    style={{ margin: 4, padding: 16 }}
+    onClick={() => navigate(url)}
   >
-    <h1 f={[1, 3]} color={'#666'}>
-      {name}
-    </h1>
-    <h2 pt={1} f={[2, 2, 4]}>
-      {theme}
-    </h2>
-  </BoxWrap>
+    <h1 style={{ fontSize: 18, margin: 2, padding: 0 }}>{name}</h1>
+    <p style={{ padding: 0, margin: 0 }}>{theme}</p>
+  </Card>
+)
+
+const SeminerCell = props => (
+  <Cell columns={4}>
+    <Seminar {...props} />
+  </Cell>
 )
 
 export default () => (
-  <Box>
-    <AppBar title="過去のイベント">
-      <Flex wrap pl={[20, 100]} pt={[10, 40]}>
-        {seminarList.map(seminar => <Seminar {...seminar} />)}
-      </Flex>
-    </AppBar>
-  </Box>
+  <AppBar title="過去のイベント">
+    <Grid>
+      <Row>{seminarList.map(SeminerCell)}</Row>
+    </Grid>
+  </AppBar>
 )
