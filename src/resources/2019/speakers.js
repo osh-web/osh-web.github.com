489b9a04cc77c3903f19b33fd85d5d428dc17b8d
Tomohiko Himura <eiel.hal@gmail.com>
2018-12-29 10:10:14 +0900

    feat: あべさんついか

--- src/resources/2019/speakers.js
+++ src/resources/2019/speakers.js
@@ -18,11 +18,25 @@ type Speaker = {
   },
 }
 
+export const mao_instantlife: Spaker = {
+  furi: 'あべ しんすけ',
+  name: '阿部 信介',
+  key: 'mao_instantlife',
+  title: 'クラスメソッド株式会社 サーバーレス開発部マネージャ',
+  description: `パチンコ店向けWin32ネイティブアプリの開発、教育系企業のシステム子会社での社内システム開発、ショッピングセンター向けソリューションの開発チームマネージャなどを経て現在。現職ではエンジニアリングマネージャとして、
+  分散受託チームのマネジメントをフルリモートでチャレンジ中。
+  前々職より岡山を中心にコミュニティに参加し始め、現在はOkayama Managers Meetingの主催や合同勉強会での発表を中心に活動している。`,
+  company: {
+    name: 'クラスメソッド株式会社 ',
+    uri: 'https://classmethod.jp/',
+  },
+}
+
 export const tatsuo310: Speaker = {
   furi: 'さとう たつお',
   name: '佐藤 達男',
   key: 'tatsuo310',
-  title: ['広島修道大学　経済科学部准教授', 'キャリアセンター長'],
+  title: ['広島修道大学 経済科学部准教授', 'キャリアセンター長'],
   description:
     'SIerとして数多くのシステム開発にプロジェクトマネジャーとして参画し、人事課長、技術部長、PM部長などを経た後、フィールドを大学に移し、現在に至る。技術経営、UX/HCD、アジャイル、プロジェクトマネジメントなどを領域横断的にカバーし、新たな価値創造型のマネジメントをテーマに研究活動を展開している。著書に「プロジェクトマネージャ育成法」（日科技連出版社）ほか。Ph.D.,PMP, PMS,CSPO, CSM',
   company: {
@@ -35,7 +49,7 @@ export const tatsuyatakemura: Speaker = {
   furi: 'たけむら たつや',
   name: '武村 達也',
   key: 'tatsuyatakemura',
-  title: ['広島県商工労働局　イノベーション推進チーム'],
+  title: ['広島県商工労働局 イノベーション推進チーム'],
   description:
     '某銀行から広島県に出向中。ハッカソン、NT広島などIT系のイベントの企画・運営および広島県のAI/IoT事業「ひろしまサンドボックス」の人材育成事業において中小企業のIoT人材育成に携わる。7年前、41歳のときに独学でiPhoneアプリを制作、現在まで12作品をリリース。うち１作品が面白法人カヤック主催クソゲー供養会で大賞を受賞。1年前、電子工作を独学で始めMakefFaire東京、台北などに出展。',
   company: {
@@ -44,4 +58,4 @@ export const tatsuyatakemura: Speaker = {
   },
 }
 
-export const speakers = [tatsuo310, tatsuyatakemura]
+export const speakers = [mao_instantlife, tatsuo310, tatsuyatakemura]
