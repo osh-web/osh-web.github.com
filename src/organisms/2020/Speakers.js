4ddd457df89b3ab8a54847ba96bf8b23b0e5e8b9
mmorito <morito@mnes.org>
2019-11-10 23:01:37 +0900

    add: organisms for 2020

---
+++ src/organisms/2020/Speakers.js
@@ -0,0 +1,63 @@
+// @flow strict
+import React from 'react'
+import Card, { CardMedia } from '@material/react-card'
+import { Button } from '@material/react-button'
+import { Cell, Grid, Row } from '@material/react-layout-grid'
+import {
+  Body2,
+  Headline1,
+  Headline2,
+  Caption,
+} from '@material/react-typography'
+
+const Speaker = ({ name, furi, description, id, title, memberships }) => (
+  <Card className="mdc-elevation--z1" outlined>
+    <CardMedia imageUrl={`/img/2020/${id}.jpg`} square />
+    <div style={{ padding: '0 16px' }}>
+      <Headline1 style={{ fontSize: 18, lineHeight: 1 }}>
+        セッション内容 TBD
+      </Headline1>
+      <Caption style={{ fontSize: 14, margin: 0, padding: 0 }}>{furi}</Caption>
+      <Headline1 style={{ fontSize: 18, lineHeight: 1, margin: 0 }}>
+        {name}
+      </Headline1>
+      <div style={{ fontSize: 16, lineHeight: 1, margin: 0, padding: '8px 0' }}>
+        {memberships.map(({ uri, name }) => (
+          <Button href={uri} style={{ height: 32, padding: '2px 0' }}>
+            {name}
+          </Button>
+        ))}
+      </div>
+      <Body2>{description}</Body2>
+      <Body2>
+        {title.reduce(
+          (acc, elem) => (
+            <span>
+              {acc}
+              <br />
+              {elem}
+            </span>
+          ),
+          [],
+        )}
+      </Body2>
+    </div>
+  </Card>
+)
+
+export const Speakers = ({ speakers }) => (
+  <Grid>
+    <Row>
+      <Cell>
+        <Headline2>Sessions</Headline2>
+      </Cell>
+    </Row>
+    <Row>
+      {speakers.map(speaker => (
+        <Cell columns={4} key={speaker.key}>
+          <Speaker {...speaker} id={speaker.key} />
+        </Cell>
+      ))}
+    </Row>
+  </Grid>
+)
---
+++ src/organisms/2020/Sponsors.js
@@ -0,0 +1,46 @@
+// @flow strict
+import React from 'react'
+import { Cell, Grid, Row } from '@material/react-layout-grid'
+import {
+    Headline2,
+} from '@material/react-typography'
+
+import { SponsorBanner as Banner } from '../../atoms/SponsorBanner'
+import type { Props as SponsorProps } from '../../atoms/SponsorBanner'
+import type { Sponsor as Resource } from '../../resources'
+
+const style = {
+    margin: '5% 10%',
+    boxSizing: 'border-box',
+};
+
+export function Sponsors({ platinum, gold, silver }: { platinum: Resource[], gold: Resource[], silver: Resource[] }) {
+  return (
+    <Grid>
+        <Row>
+            <Cell>
+                <Headline2>Sponsors</Headline2>
+            </Cell>
+        </Row>
+      <Row>
+          {platinum.map((sponsor) =>(
+              <Cell columns={8} style={style}>{Banner(toSponsor(sponsor))}</Cell>
+          ))}
+      </Row>
+      <Row>
+          {gold.map((sponsor) =>(
+              <Cell columns={3} desktopColumns={6} style={style}>{Banner(toSponsor(sponsor))}</Cell>
+          ))}
+      </Row>
+        <Row>
+            {silver.map((sponsor) =>(
+                <Cell columns={2} desktopColumns={3} style={style}>{Banner(toSponsor(sponsor))}</Cell>
+            ))}
+        </Row>
+    </Grid>
+  )
+}
+
+function toSponsor({ banner, url, ...args }: Resource): SponsorProps {
+  return { src: banner, href: url, ...args }
+}
