9b88a7d05940e6964049139f88edf4445a5c0b79
mmorito <morito@mnes.org>
2020-01-22 21:17:30 +0900

    Format Document

--- src/atoms/SponsorBanner.js
+++ src/atoms/SponsorBanner.js
@@ -1,17 +1,21 @@
 // @flow strict
-import React from 'react';
+import React from 'react'
 
 export type Props = {
-    href: string,
-    src: string,
-    name: string,
-    height: Number,
+  href: string,
+  src: string,
+  name: string,
+  height: Number,
 }
 
-export function SponsorBanner({name, href, src, height }: Props) {
-    return (
-        <a href={href}>
-            <img src={src} alt={`${name} バナー`} style={{objectFit: 'contain', width: "100%", height: height}}/>
-        </a>
-    )
+export function SponsorBanner({ name, href, src, height }: Props) {
+  return (
+    <a href={href}>
+      <img
+        src={src}
+        alt={`${name} バナー`}
+        style={{ objectFit: 'contain', width: '100%', height: height + 'px' }}
+      />
+    </a>
+  )
 }
