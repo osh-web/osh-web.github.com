3aa3239f0afe6ed5aa132887c3884dcf756bcd7a
Yasunari Tanaka <yasu7ri@users.noreply.github.com>
2025-04-29 10:17:19 +0900

    スタッフ情報を更新し、主要メンバーを追加

--- src/resources/2025/staff.js
+++ src/resources/2025/staff.js
@@ -9,35 +9,17 @@ type Staff = {
   title?: string,
 }
 
-// 2024年からの継続スタッフとして、主要メンバーを仮定義しています
-// 実際のスタッフ編成が決まり次第、更新してください
-
-const mmorito = {
-  key: 'mmorito',
-  name: 'mmorito',
-  title: '実行委員長',
-  twitter: 'mmorito_0318',
-}
-
-const nishimotz = {
-  key: '24motz',
-  name: '西本 卓也',
-  twitter: '24motz',
-  facebook: 'takuya.nishimoto',
-  uri: 'https://shuaruta.com/',
-}
-
-const isabasi1484 = {
-  key: 'isabasi1484',
-  name: '石橋 伸介',
-  twitter: 'isabisi1484',
-  github: 'isabisi1484',
-}
-
 const kakenavi = {
   key: 'kakenavi',
   name: '山城戸 祐樹',
   twitter: 'kakenavi',
+}
+
+const mu2in = {
+  key: 'mu2in',
+  name: 'mu2in',
+  twitter: 'mu2in',
+  github: 'mu2in',
 }
 
 const m_ando_japan = {
@@ -48,12 +30,100 @@ const m_ando_japan = {
   uri: 'https://bitriver.jp/',
 }
 
-// スタッフ一覧
-// 実際のスタッフが確定次第、更新してください
+const nanarichi = {
+  key: '7richi',
+  name: '田中 靖也',
+  twitter: '7richi',
+  github: 'yasu7ri',
+  facebook: 'yasu7ri',
+}
+
+const isabasi1484 = {
+  key: 'isabasi1484',
+  name: '石橋 伸介',
+  twitter: 'isabisi1484',
+  github: 'isabisi1484',
+}
+
+const nishimotz = {
+  key: '24motz',
+  name: '西本 卓也',
+  twitter: '24motz',
+  facebook: 'takuya.nishimoto',
+  uri: 'https://shuaruta.com/',
+}
+
+const mmorito = {
+  key: 'mmorito',
+  name: 'mmorito',
+  twitter: 'mmorito_0318',
+}
+
+const hiroyuki = {
+  key: 'hiroyuki',
+  name: '前田 浩幸',
+  twitter: 'hiroyukimaeda_3',
+}
+
+const naoyuki = {
+  key: 'naoyuki',
+  name: '濱田 直之',
+  twitter: 'Naoyukisc2',
+}
+
+const mosugi = {
+  key: 'mosugi',
+  name: 'Masaki Osugi',
+}
+
+const yoshida = {
+  key: 'yoshida',
+  name: '吉田 豪',
+}
+
+const yamamoto = {
+  key: 'yamamoto',
+  name: '山本 直輝',
+  twitter: 'ymnk4919',
+  github: 'mtbk4919naoki',
+  facebook: 'ymnk4919',
+}
+
+const ozaki = {
+  key: 'ozaki',
+  name: '尾崎 徳生',
+  facebook: 'norio.ozaki.711',
+}
+
+const kaneyasu = {
+  title: '実行委員長',
+  key: 'kaneyasu',
+  name: '兼安 聡',
+  twitter: 'satoshi256kbyte',
+  github: 'satoshi256kbyte',
+  facebook: 'kaneyasusatoshi',
+}
+
+const sugiyama = {
+  key: 'sugiyama',
+  name: '杉山 花代',
+  twitter: 'Bluerose_87',
+}
+
 export const staff: Staff[] = [
+  kaneyasu,
   mmorito,
   nishimotz,
   isabasi1484,
   kakenavi,
   m_ando_japan,
+  nanarichi,
+  mu2in,
+  // mosugi,
+  hiroyuki,
+  yoshida,
+  yamamoto,
+  ozaki,
+  sugiyama,
+  naoyuki,
 ]
